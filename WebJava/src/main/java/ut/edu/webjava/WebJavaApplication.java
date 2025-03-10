ffe17752b7b404fb36875485539dc05a8ec8ec06
Thắng <thangnh5428@ut,edu.vn>
2025-03-10 18:42:49 +0700

    Commit

---
+++ WebJava/src/main/java/ut/edu/webjava/WebJavaApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.webjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJavaApplication.class, args);
+	}
+
+}
---
+++ WebJava/src/test/java/ut/edu/webjava/WebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.webjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
