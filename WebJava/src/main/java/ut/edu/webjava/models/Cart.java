9201feebec774fffca5ca268330ef9d53d241dee
thinhtnp <thinhtnp2347@ut.edu.vn>
2025-03-29 14:00:37 +0700

    them chuc nang cho gio hang

--- WebJava/src/main/java/ut/edu/webjava/models/Cart.java
+++ WebJava/src/main/java/ut/edu/webjava/models/Cart.java
@@ -2,6 +2,7 @@ package ut.edu.webjava.models;
 
 import jakarta.persistence.*;
 import java.util.HashSet;
+import java.util.Objects; // Thêm để hỗ trợ equals() và hashCode()
 import java.util.Set;
 
 @Entity
@@ -12,10 +13,10 @@ public class Cart {
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false) // Đảm bảo user không null
     private User user;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY) //Thêm fetch = FetchType.LAZY để tối ưu
     private Set<CartItem> items = new HashSet<>();
 
     public Cart() {}
@@ -32,6 +33,10 @@ public class Cart {
         return user;
     }
 
+    public void setUser(User user) { // Thêm setter cho user
+        this.user = user;
+    }
+
     public Set<CartItem> getItems() {
         return items;
     }
@@ -44,5 +49,25 @@ public class Cart {
     public void removeItem(CartItem item) {
         items.remove(item);
         item.setCart(null);
+    }
+
+    public void clearCart() { // Thêm phương thức xóa toàn bộ giỏ hàng
+        for (CartItem item : items) {
+            item.setCart(null);
+        }
+        items.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) { //  Cập nhật equals()
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart cart = (Cart) o;
+        return Objects.equals(id, cart.id) && Objects.equals(user, cart.user);
+    }
+
+    @Override
+    public int hashCode() { // Cập nhật hashCode()
+        return Objects.hash(id, user);
     }
 }
