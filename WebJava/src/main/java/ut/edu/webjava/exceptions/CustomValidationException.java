eba1408471ac0838cabb27da4541287342f5fa1e
LamVanTi <tilv2346@ut.edu.vn>
2025-03-30 19:45:34 +0700

    package exceptions

---
+++ WebJava/src/main/java/ut/edu/webjava/exceptions/CustomValidationException.java
@@ -0,0 +1,21 @@
+package ut.edu.webjava.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CustomValidationException extends RuntimeException {
+    private final Map<String, String> errors;
+
+    public CustomValidationException(Map<String, String> errors) {
+        super("Validation failed");
+        this.errors = errors;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+}
+
