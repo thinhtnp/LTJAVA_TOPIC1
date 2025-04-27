3ffd66adde6ad92068f657244656c9a5da7dfca5
NguyenCongTai <tainc2535@ut.edu.vn>
2025-04-27 18:35:58 +0700

    add repo

--- WebJava/src/main/java/ut/edu/webjava/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ut.edu.webjava.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import ut.edu.webjava.models.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
-
--- WebJava/src/main/java/ut/edu/webjava/repositories/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package ut.edu.webjava.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ut.edu.webjava.models.Review;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
