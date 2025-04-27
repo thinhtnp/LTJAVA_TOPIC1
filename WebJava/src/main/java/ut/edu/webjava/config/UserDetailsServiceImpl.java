4bfc914d1f4184c001a3b3710072dd7f352e683d
NguyenCongTai <tainc2535@ut.edu.vn>
2025-04-27 18:35:21 +0700

    model update

---
+++ WebJava/src/main/java/ut/edu/webjava/config/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package ut.edu.webjava.config;
+
+import ut.edu.webjava.models.UserDtls;
+import ut.edu.webjava.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		UserDtls user = userRepository.findByEmail(username);
+
+		if (user == null) {
+			throw new UsernameNotFoundException("user not found");
+		}
+		return new CustomUser(user);
+	}
+
+}
