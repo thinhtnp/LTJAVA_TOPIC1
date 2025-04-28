b70349bacf61183a76b4a031342390846386ac85
NguyenCongTai <tainc2535@ut.edu.vn>
2025-04-28 13:04:37 +0700

    update

--- WebJava/src/main/java/ut/edu/webjava/util/CommonUtil.java
+++ WebJava/src/main/java/ut/edu/webjava/util/CommonUtil.java
@@ -30,7 +30,7 @@ public class CommonUtil {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
-        helper.setFrom("daspabitra55@gmail.com", "Shooping Cart");
+        helper.setFrom("congtai308@gmail.com", "Shooping Cart");
         helper.setTo(reciepentEmail);
 
         String content = "<p>Hello,</p>" + "<p>You have requested to reset your password.</p>"
@@ -67,7 +67,7 @@ public class CommonUtil {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
-        helper.setFrom("daspabitra55@gmail.com", "Shooping Cart");
+        helper.setFrom("congtai308@gmail.com", "Shooping Cart");
         helper.setTo(order.getOrderAddress().getEmail());
 
         msg=msg.replace("[[name]]",order.getOrderAddress().getFirstName());
